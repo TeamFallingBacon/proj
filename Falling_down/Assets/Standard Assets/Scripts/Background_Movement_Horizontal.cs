ddfbfad75c8de7e989c72cba6f5ce586d334789f
TeamFallingBacon <xces2kill@gmail.com>
2014-01-19 02:11:48 -0800

    Level 2

---
+++ Falling_down/Assets/Standard Assets/Scripts/Background_Movement_Horizontal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background_Movement_Horizontal : MonoBehaviour {
+	public GameObject debris;
+	private float spawnPosition;
+	public bool shouldSpawn;
+	// Use this for initialization
+	void Start () {
+		spawnPosition = 10f;
+		StartCoroutine(RandomSpawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public IEnumerator RandomSpawn () {
+		while(shouldSpawn) {
+			float movementSpeed = Random.Range(.08f,.12f);
+			yield return new WaitForSeconds(.25f);
+			GameObject instance = (GameObject)Instantiate(debris,new Vector2(spawnPosition,Random.Range(-4.66f,4.66f)), Quaternion.Euler(0,0,90));
+			Falling_Script_Horizontal fallScript = (Falling_Script_Horizontal)instance.GetComponent(typeof(Falling_Script_Horizontal));
+			fallScript.movementSpeed = movementSpeed;
+		}
+	}
+}
---
+++ Falling_down/Assets/Standard Assets/Scripts/Falling_Script_Horizontal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Falling_Script_Horizontal : MonoBehaviour {
+	private GameObject fallingObject;
+	public bool generateSpeed = false;
+	public float movementSpeed = 0;
+	private Vector3 movementAmount;
+	// Use this for initialization
+	void Start () {
+		fallingObject = gameObject;
+		if (generateSpeed)
+			movementSpeed = Random.Range(.08f,.12f);
+		movementAmount = new Vector3(movementSpeed,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fallingObject.transform.position -= movementAmount * Time.deltaTime*100;
+	}
+}
