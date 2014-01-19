d35a27fb8c936462a00c13107ac1e1e48d1ca8ba
Peter Anteater <panteat@uci.edu>
2014-01-18 19:09:07 -0800

    Blank

--- Falling_down/Assets/Standard Assets/Scripts/Player_Movement.cs
+++ Falling_down/Assets/Standard Assets/Scripts/Player_Movement.cs
@@ -5,10 +5,15 @@ public class Player_Movement : MonoBehaviour {
 	public float movementSpeed = 0;
 	private Vector3 movementAmountX;
 	private GameObject player;
+	public float hoverAmount;
+	public bool shouldHover = true;
+	public float hoverLimits = 10f;
+
 	// Use this for initialization
 	void Start () {
 		movementAmountX = new Vector3(movementSpeed, 0, 0);
 		player = gameObject;
+		StartCoroutine(Hover(hoverAmount));
 	}
 	
 	// Update is called once per frame
@@ -17,6 +22,14 @@ public class Player_Movement : MonoBehaviour {
 			player.transform.position -= movementAmountX * Time.deltaTime*80;
 		} else if (Input.GetKey("right") && player.transform.position.x <= 12.3f) {
 			player.transform.position += movementAmountX * Time.deltaTime*80;
+		}
+	}
+
+	void IEnumerator Hover(float hoverAmount) {
+		while (shouldHover) {
+			int dir = Random.Range(0,1);
+			if (dir && ) { //Up
+				player.transform.Translate(
 		}
 	}
 }
