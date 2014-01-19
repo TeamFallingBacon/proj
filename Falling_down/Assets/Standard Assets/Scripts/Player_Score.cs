f355fd40b06bb33bdc80e0e5826deaa929dbeddc
TeamFallingBacon <xces2kill@gmail.com>
2014-01-18 21:55:04 -0800

    Score, timer and player score

---
+++ Falling_down/Assets/Standard Assets/Scripts/Player_Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Score : MonoBehaviour {
+	public int score = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Portal") {
+			Debug.Log("Score + 100");
+			score += 100;
+		}
+	}
+}
---
+++ Falling_down/Assets/Standard Assets/Scripts/Score_GUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score_GUI : MonoBehaviour {
+	public GUISkin guiSkin;
+	public GameObject player;
+	public Player_Score score;
+	// Use this for initialization
+	void Start () {
+		score = (Player_Score) player.GetComponent(typeof(Player_Score));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.skin = guiSkin;
+		string scoreText = string.Format ("Score: {0}", score.score);
+		GUI.Label (new Rect (10, 30, 500, 100), scoreText);
+	}
+}
