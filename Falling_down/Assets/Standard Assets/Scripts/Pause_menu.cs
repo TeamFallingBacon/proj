5c681d57a21c10fd0e277b7eced2e6da9c07ea41
TeamFallingBacon <xces2kill@gmail.com>
2014-01-18 18:53:36 -0800

    Pause and gameover

--- Falling_down/Assets/Standard Assets/Scripts/Pause_menu.cs
+++ Falling_down/Assets/Standard Assets/Scripts/Pause_menu.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Pause_menu : MonoBehaviour {
 	public GUISkin guiSkin;
+	public GameObject camera;
+	public FuelGauge fuel;
 	void pause(){
 		Time.timeScale = 0.0f;
 	}
@@ -20,7 +22,8 @@ public class Pause_menu : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-	
+		unpause ();
+		fuel = (FuelGauge) camera.GetComponent(typeof(FuelGauge));
 	}
 	
 	// Update is called once per frame
@@ -32,8 +35,20 @@ public class Pause_menu : MonoBehaviour {
 	}
 	void OnGUI(){
 		GUI.skin = guiSkin;
-		if (Time.timeScale < 1) {
-			GUI.Label (new Rect (Screen.width/2 - 62.0f, Screen.height/2-25.0f, 150, 50), "PAUSED");		
+		if (fuel.gameOver) {
+			Debug.Log ("Before restart");
+			Debug.Log (Time.timeScale);
+			pause ();
+			GUI.Label (new Rect (Screen.width/2-100.0f, Screen.height/2-25.0f, 250, 50), "GAME OVER");
+			if (GUI.Button(new Rect (Screen.width/2 - 62.0f, Screen.height/2+25.0f, 150, 50), "RESTART")) {
+				Application.LoadLevel("level1");
+			}
+		}
+		
+		else if (Time.timeScale < 1) {
+			Debug.Log ("in pause thingy");
+			Debug.Log (Time.timeScale);
+			GUI.Label (new Rect (Screen.width/2 - 65.0f, Screen.height/2-25.0f, 170, 50), "PAUSED"); 
 		}
 	}
 }
