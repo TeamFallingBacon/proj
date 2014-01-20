ccd11f68a7ec0a17188b07462aadbaf6b4822b7e
Peter Anteater <panteat@uci.edu>
2014-01-19 22:30:50 -0800

    Sounds and Title screen fix!

---
+++ Falling_down/Assets/Standard Assets/Scripts/Soundtrack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Soundtrack : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Playyyy!");
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
