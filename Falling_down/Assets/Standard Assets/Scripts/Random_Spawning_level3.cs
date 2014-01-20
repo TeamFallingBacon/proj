ace7c37b11cbc86d2c71823dd118927c769de7bc
TeamFallingBacon <xces2kill@gmail.com>
2014-01-19 22:31:09 -0800

    Level 2 and Level 3

--- Falling_down/Assets/Standard Assets/Scripts/Random_Spawning_level3.cs
+++ Falling_down/Assets/Standard Assets/Scripts/Random_Spawning_level3.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Random_Spawning_Horizontal : MonoBehaviour {
+public class Random_Spawning_level3 : MonoBehaviour {
 	public GameObject portal, item, asteroid;
 	public bool shouldSpawn;
 	private float spawnPosition;
+	
 	// Use this for initialization
 	void Start () {
 		spawnPosition = 8f;
-		//StartCoroutine(RandomSpawn(3.0f));
 		StartCoroutine(ItemSpawn(Random.Range(0.01f, 2.0f)));
 		StartCoroutine(AsteroidSpawn(Random.Range(0.04f, 2.0f)));
 		StartCoroutine(PortalSpawn(Random.Range(3.0f, 6.0f)));
@@ -16,21 +16,14 @@ public class Random_Spawning_Horizontal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-	/*
-	public IEnumerator RandomSpawn (float waitTime) {
-		while(shouldSpawn) {
-			yield return new WaitForSeconds(waitTime);
-			Instantiate(portal,new Vector2(spawnPosition,Random.Range(-4.66f,4.66f)), Quaternion.Euler(0,0,90));
-		}
-	}*/
-
+	
 	public IEnumerator AsteroidSpawn (float waitTime) {
 		while(shouldSpawn) {
 			for (int i = 0; i != Random.Range(1, 12); i++) {
 				yield return new WaitForSeconds(Random.Range(0.01f, 1.2f));
-				Instantiate(asteroid,new Vector2(spawnPosition,Random.Range(-4.66f,4.66f)), Quaternion.Euler(0,0,90));
+				Instantiate(asteroid,new Vector2(Random.Range(2.996f,12.3f),spawnPosition), Quaternion.identity);
 			}
 			waitTime = Random.Range(0.01f, 2.0f);
 		}
@@ -40,7 +33,7 @@ public class Random_Spawning_Horizontal : MonoBehaviour {
 		while(shouldSpawn) {
 			for (int i = 0; i != Random.Range(6, 9); i++) {
 				yield return new WaitForSeconds(Random.Range(0.01f, 1.2f));
-				Instantiate(item,new Vector2(spawnPosition,Random.Range(-4.66f,4.66f)), Quaternion.Euler(0,0,90));
+				Instantiate(item,new Vector2(Random.Range(2.996f,12.3f),spawnPosition), Quaternion.identity);
 			}
 			waitTime = Random.Range(0.04f, 2.0f);
 		}
@@ -49,7 +42,7 @@ public class Random_Spawning_Horizontal : MonoBehaviour {
 	public IEnumerator PortalSpawn (float waitTime) {
 		while(shouldSpawn) {
 			yield return new WaitForSeconds(waitTime);
-			Instantiate(portal,new Vector2(spawnPosition,Random.Range(-4.66f,4.66f)), Quaternion.Euler(0,0,90));
+			Instantiate(portal,new Vector2(Random.Range(2.996f,12.3f),spawnPosition), Quaternion.identity);
 			waitTime = Random.Range(3.0f, 6.0f);
 		}
 	}
