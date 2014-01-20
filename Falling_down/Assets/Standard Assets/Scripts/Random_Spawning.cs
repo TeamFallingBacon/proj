2e4dbeff0e5c16a9b0caa75d0293f76d2e977cd0
TeamFallingBacon <xces2kill@gmail.com>
2014-01-19 23:11:32 -0800

    Incomplete Clock

--- Falling_down/Assets/Standard Assets/Scripts/Random_Spawning.cs
+++ Falling_down/Assets/Standard Assets/Scripts/Random_Spawning.cs
@@ -12,6 +12,7 @@ public class Random_Spawning : MonoBehaviour {
 		StartCoroutine(ItemSpawn(Random.Range(0.01f, 2.0f)));
 		StartCoroutine(AsteroidSpawn(Random.Range(0.04f, 2.0f)));
 		StartCoroutine(PortalSpawn(Random.Range(3.0f, 6.0f)));
+		StartCoroutine (ClockSpawn (Random.Range (7.0f, 10.0f)));
 	}
 	
 	// Update is called once per frame
@@ -44,6 +45,14 @@ public class Random_Spawning : MonoBehaviour {
 			yield return new WaitForSeconds(waitTime);
 			Instantiate(portal,new Vector2(Random.Range(2.996f,12.3f),spawnPosition), Quaternion.identity);
 			waitTime = Random.Range(3.0f, 6.0f);
+		}
+	}
+
+	public IEnumerator ClockSpawn (float waitTime) {
+		while (shouldSpawn) {
+			yield return new WaitForSeconds(waitTime);
+			Instantiate(portal,new Vector2(Random.Range(2.996f,12.3f),spawnPosition), Quaternion.identity);
+			waitTime = Random.Range(7.0f, 10.0f);
 		}
 	}
 }
