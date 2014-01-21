04af4fa353dbd0d28a3f3ec0d76abc90891a828c
TeamFallingBacon <xces2kill@gmail.com>
2014-01-20 17:50:17 -0800

    level 2 and main menu forced resolution

---
+++ Falling_down/Assets/Standard Assets/Scripts/ScreenResolution.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Awake(){
+		Screen.SetResolution (500, 500, false);
+	}
+}
